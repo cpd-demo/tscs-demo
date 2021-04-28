ff9970bcde425e164f1f2431cdcddf2da83515a1
LaplaceMan <74650892+LaplaceMan@users.noreply.github.com>
2021-04-28 18:35:48 +0800

    Update FlowOracle.sol

--- Subtitle System/Multiple-Subtitle/FlowOracle.sol
+++ Subtitle System/Multiple-Subtitle/FlowOracle.sol
@@ -30,7 +30,7 @@ contract FlowOracle {
         uint subnewflow;
     }
     mapping(uint => VideoInfo) Videos;
-    mapping(uint => mapping(string => bool)) clearThrough;
+    //mapping(uint => mapping(string => bool)) clearThrough;
     event FlowUpdate(string videoname,string videosource,uint webindex,uint newupdate,uint flow);
     uint WhitelistNumber;
     mapping(address => bool) Whitelist;
@@ -80,9 +80,9 @@ contract FlowOracle {
         require(Videos[webindex].videowner == usr);
         return true;
     }
-    function confirmThrough(uint _webindex,string memory _simhash)external onlyoracleAddress returns(bool){
-        clearThrough[_webindex][_simhash] = true;
-        return true;
-    }
+    //function confirmThrough(uint _webindex,string memory _simhash)external onlyoracleAddress returns(bool){
+    //    clearThrough[_webindex][_simhash] = true;
+    //    return true;
+    //}
 
 }
